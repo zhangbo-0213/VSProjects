4d191a9b85f67f8ab9505a7759108150db10ecb1
bo_zhang <bo_zhang1993@163.com>
2017-08-17 10:08:05 +0800

    add in WPF_Study

---
+++ WPF_Study/P37/About_X_Type/MyButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace About_X_Type
+{
+    class MyButton:Button
+    {
+        public Type UserWindowType { get; set; }
+
+        protected override void OnClick()
+        {
+            base.OnClick();//激发Click事件
+            UserControl win=Activator.CreateInstance(this.UserWindowType) as UserControl;
+            //MessageBox.Show("done");
+            if (win != null)
+            {
+                Window window=new Window();
+                window.Width = 300;
+                window.Height = 300;
+                window.Content = win;
+                window.ShowDialog();
+            }
+        }
+    }
+}
